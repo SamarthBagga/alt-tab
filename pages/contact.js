abebe8c0e7239e6edaede72c5c8d5af2aee6d34b
Samarth Bagga <samarthbagga@gmail.com>
2023-04-16 19:30:24 +0530

    contact-page-final?

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,8 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import Navbar from "../src/components/Navbar";
 import { useRouter } from "next/router";
 import Footer from "../src/components/Footer";
-import { useState } from "react";
 
 function Contact() {
   const form = useRef();
@@ -30,88 +29,92 @@ function Contact() {
         }
       );
   };
+
   const [options, setOptions] = useState({
     option1: false,
     option2: false,
-    option3: false
+    option3: false,
   });
 
   const handleCheckboxChange = (event) => {
-    const { name, checked } = event.target;
-    setOptions({ ...options, [name]: checked });
+    setOptions({
+      ...options,
+      [event.target.name]: event.target.checked,
+    });
   };
 
+  const { option1, option2, option3 } = options;
 
   return (
-      <section className='mentor-section'>
-        <Navbar/>
-        <div className='sections mentor flex items-center'>
-        
-        <div className='flex flex-col items-center w-[80%] mx-auto mt-10 boxes-2'>
-          <div className='mt-20'>
-          <h1 className='text-left text-4xl font-4xl'>Contact Us</h1>
-          <form ref={form} onSubmit={sendEmail}>
-          <p>
-            Please take a moment to get in touch, we will get back to you
-            shortly.
-          </p>
+    <section className="mentor-section">
+      <Navbar />
+      <div className="sections mentor flex items-center">
+        <div className="flex flex-col items-center w-[80%] mx-auto mt-10 boxes-2">
+          <div className="mt-20">
+            <h1 className="text-left text-4xl font-4xl">Contact Us</h1>
+            <form ref={form} onSubmit={sendEmail}>
+              <p>Please take a moment to get in touch, we will get back to you shortly.</p>
 
-          <div class="column flex flex-col">
-            <label for="the-name">Your Name</label>
-            <input type="text" name="user_name" id="the-name" />
+              <div className="column flex flex-col">
+                <label htmlFor="the-name">Your Name</label>
+                <input type="text" name="user_name" id="the-name" />
 
-            <label for="the-email">Email Address</label>
-            <input type="email" name="user_email" id="the-email" />
+                <label htmlFor="the-email">Email Address</label>
+                <input type="email" name="user_email" id="the-email" />
 
-            <label for="the-phone">Phone Number</label>
-            <input type="tel" name="phone_num" id="the-phone" />
-          
-            <label for="the-message">Message</label>
-            <textarea name="message" id="the-message"></textarea>
+                <label htmlFor="the-phone">Phone Number</label>
+                <input type="tel" name="phone_num" id="the-phone" />
 
-            <div>
-      <label>
-        <input
-          type="checkbox"
-          name="option1"
-          checked={options.option1}
-          onChange={handleCheckboxChange}
-        />
-        Option 1
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          name="option2"
-          checked={options.option2}
-          onChange={handleCheckboxChange}
-        />
-        Option 2
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          name="option3"
-          checked={options.option3}
-          onChange={handleCheckboxChange}
-        />
-        Option 3
-      </label>
-    </div>
+                <label htmlFor="the-options">Select options</label>
+                <div id="the-options">
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="option1"
+                      checked={option1}
+                      onChange={handleCheckboxChange}
+                    />
+                    Mentor
+                  </label>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="option2"
+                      checked={option2}
+                      onChange={handleCheckboxChange}
+                    />
+                    Recruit
+                  </label>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="option3"
+                      checked={option3}
+                      onChange={handleCheckboxChange}
+                    />
+                    Other
+                  </label>
+                </div>
 
-            <button className=" mt-3 send click" type="submit">
-              Send Details
-            </button>
+                <label htmlFor="the-message">Message</label>
+                <textarea
+                  name="message"
+                  id="the-message"
+                  value={`Selected options: ${option1 ? "Mentor, " : ""}${
+                    option2 ? "Recruit, " : ""
+                  }${option3 ? "Other" : ""}`}
+                ></textarea>
+
+                <button className="mt-3 send click" type="submit">
+                  Send Details
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
-</div>
-          
-
         </div>
-        </div>
-        <Footer/>
+      </div>
+      <Footer />
     </section>
-  );
-}
-
-export default Contact;
+ 
+  )};
+  export default Contact
--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import { useState } from 'react';
 
 export const ContactUs = () => {
   const form = useRef();
@@ -15,16 +16,81 @@ export const ContactUs = () => {
           console.log(error.text);
       });
   };
+  const [options, setOptions] = useState({
+    option1: false,
+    option2: false,
+    option3: false,
+  });
+
+  const handleOptionChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    setOptions({
+      ...options,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    // Combine the selected options into the message text
+    const message = `Selected options: 
+      Option 1: ${options.option1}
+      Option 2: ${options.option2}
+      Option 3: ${options.option3}`;
+
+    // Send the message using emailjs
+    // ...
+    emailjs.sendForm('service_x3zid9d', 'template_y6biong', form.current, 'gJvlNiZo3mc0qLlvf')
+      .then((result) => {
+          console.log(result.text);
+          console.log("messgae sent");
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
+<form onSubmit={handleFormSubmit}>
       <label>Name</label>
       <input type="text" name="user_name" />
       <label>Email</label>
       <input type="email" name="user_email" />
+      <label>Options</label>
+      <label>
+        <input
+          type="checkbox"
+          name="option1"
+          checked={options.option1}
+          onChange={handleOptionChange}
+        />
+        Mentor
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="option2"
+          checked={options.option2}
+          onChange={handleOptionChange}
+        />
+        Recruiter
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="option3"
+          checked={options.option3}
+          onChange={handleOptionChange}
+        />
+        Other
+      </label>
       <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="send"></input>
+      <textarea name="message" value={message} />
+      <input type="submit" value="send" />
     </form>
+
   );
 };
